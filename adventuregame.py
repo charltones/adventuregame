c5f29d507fb8f95adb581d77bbd52c33e398012f
Charlton <rccharlt@acbc32951deb.ant.amazon.com>
2020-06-07 18:16:11 +0100

    Hari's first attempt

--- adventuregame.py
+++ adventuregame.py
@@ -1,5 +1,9 @@
 from adventurelib import *
 
+Item.immobile = False
+Room.items = Bag()
+inventory = Bag()
+
 @when('north', direction='north')
 @when('south', direction='south')
 @when('east', direction='east')
@@ -16,65 +20,40 @@ def go(direction):
         print(f'You go {direction}.')
         look()
 
-space = Room("""
-You are drifting in space. It feels very cold.
+@when('look')
+def look():
+    say(current_room)
+    if current_room.items:
+        for i in current_room.items:
+            say('A %s is here.' % i)
 
-A slate-blue spaceship sits completely silently to your left,
-its airlock open and waiting.
-""")
-
-spaceship = Room("""
-The bridge of the spaceship is shiny and white, with thousands
-of small, red, blinking lights.
-""")
-
-# current_room will be a global variable. Let's start out in
-# space, so assign the 'space' room from above.
-current_room = space
-
-
-@when('enter airlock')
-def enter_spaceship():
-    # To set a global variable from within a function you have
-    # to include the 'global' keyword, to avoid creating a
-    # local variable instead.
-    global current_room
-
-    # Got to check if this action can be done here
-    if current_room is not space:
-        print('There is no airlock here.')
-        return
-
-    current_room = spaceship
-
-    # You should include some narrative for every action to
-    # ensure the transition doesn't feel abrupt.
-    print(
-        "You heave yourself into the airlock and slam your " +
-        "hand on the button to close the outer door."
-    )
-
-    # Show the room description to indicate we have arrived.
-    print(current_room)
-
-
-Room.can_scream = True  # The default for all rooms
-space.can_scream = False  # Set a value for a specific room.
-
-@when('scream')
-def scream():
-    if current_room.can_scream:
-        print(
-            "You unleash a piercing shriek that " +
-            "reverberates around you."
-        )
+@when('take ITEM')
+def take(item):
+    obj = current_room.items.take(item)
+    if obj and not obj.immobile:
+        say('You pick up the %s.' % obj)
+        inventory.add(obj)
+    elif obj:
+        say('It is a bit too big!')
     else:
-        print(
-            "You try to yell but there's no sound " +
-            "in the vacuum of space."
-        )
+        say('There is no %s here.' % item)
+        
+@when('drop THING')
+def drop(thing):
+    obj = inventory.take(thing)
+    if not obj:
+        say('You do not have a %s.' % thing)
+    else:
+        say('You drop the %s.' % obj)
+        current_room.items.add(obj)
 
-
+@when('inventory')
+@when('i')
+def show_inventory():
+    say('You have:')
+    for thing in inventory:
+        say(thing)
+        
 @when("brush teeth")
 def brush_teeth():
     say("""
@@ -85,5 +64,143 @@ def brush_teeth():
         run your tongue over them.
     """)
 
-print(current_room)
+#############################################################
+    
+main_cart_pass = Room("""
+    Main cart pass description. There is a dark passageway to the east and a tunnel to the south.
+""")
+
+goblet = Item('jewel encrusted goblet', 'goblet')
+rock = Item('huge old rock', 'rock')
+rock.immobile = True
+main_cart_pass.items.add(goblet)
+main_cart_pass.items.add(rock)
+
+rat_chamber = Room("""
+    Rat chamber description
+""")
+
+main_cart_pass.east = rat_chamber
+rat_chamber.west = main_cart_pass
+
+storage_room = Room("""
+    storage room description
+""")
+
+rat_chamber.north = storage_room
+storage_room.south = rat_chamber
+
+tigers_lair = Room("""
+    tigers lair description
+""")
+
+tiger = Item('hungry and rapacious sabre-tooth tiger', 'tiger')
+tiger.immobile = True
+tigers_lair.items.add(tiger)
+
+storage_room.west = tigers_lair
+tigers_lair.east = storage_room
+
+hole = Room("""
+    hole description
+""")
+
+storage_room.north = hole
+hole.south = storage_room
+
+inferno_room = Room("""
+    inferno room description
+""")
+
+hole.north = inferno_room
+inferno_room.south = hole
+
+monster_battle = Room("""
+    monster battle description
+""")
+
+gate_key = Item('rusty steel key', 'steel key')
+monster_battle.items.add(gate_key)
+
+hole.east = monster_battle
+monster_battle.west = hole
+
+trap_room = Room("""
+    trap room description
+""")
+
+end_key = Item('old iron key', 'iron key')
+trap_room.items.add(end_key)
+
+inferno_room.west = trap_room
+trap_room.east = inferno_room
+
+gate = Room("""
+    gate description
+""")
+
+main_cart_pass.south = gate
+gate.north = main_cart_pass
+
+key = Room("""
+    key description
+""")
+
+gate.south = key
+key.north = gate
+
+blue_room = Room("""
+    blue room description
+""")
+
+key.east = blue_room
+blue_room.west = key
+
+rail_01 = Room("""
+    rail 01 description
+""")
+
+blue_room.south = rail_01
+rail_01.north = blue_room
+
+rail_02 = Room("""
+    rail 02 description
+""")
+
+rail_01.west = rail_02
+rail_02.east = rail_01
+
+railend = Room("""
+    railend description
+""")
+
+rail_02.north = railend
+railend.south = rail_02
+
+disc_battle = Room("""
+    disc battle description
+""")
+
+railend.west = disc_battle
+disc_battle.east = railend
+
+ultros = Room("""
+    ultros description
+""")
+
+disc_battle.north = ultros
+ultros.south = disc_battle
+
+end = Room("""
+    Well done! You have escaped the MasterMines.
+""")
+
+ultros.north = end
+end.south = ultros
+
+#############################################################
+
+current_room = main_cart_pass
+
+look()
 start()
